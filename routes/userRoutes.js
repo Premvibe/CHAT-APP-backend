7cdc14bc6b5af53d1c1b317e79deb23915d1d130
Prem <prem7736k@gmail.com>
2025-07-27 10:25:07 +0530

    user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { login, signup, updateProfile } from '../controllers/userController';
+import { protectRoute } from '../middleware/auth.js';
+
+const userRouter = express.Router();
+
+userRouter.post("/signup",signup);
+userRouter.post("/login", login);
+userRouter.put("/upadte-profile", protectRoute,updateProfile)
+userRouter.get("/check", protectRoute,updateProfile)
