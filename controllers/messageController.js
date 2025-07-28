3758b415b21bb1db4c304a86a43ced20b5fad3d8
Prem <prem7736k@gmail.com>
2025-07-28 20:50:04 +0530

    msg controller

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -6,9 +6,7 @@ export const getUsersForSidebar = async (req, res) => {
     const userId = req.user._id;
     const users = await User.find({ _id: { $ne: userId } }).select("-password");
 
-    res.json({
-      success: true,
-      users,
-      message: "Users fetched successfully",
+  //count number of messages not seen
+    
     });
   }
